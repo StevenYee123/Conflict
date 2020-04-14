5bc74ca5848408a4826fa6fb00da61f29bb5ec1c
Steven Yee <s.yee615@gmail.com>
2020-04-14 00:07:22 -0400

    Fixed websocket type

--- app/jobs/chat_message_creation_event_broadcast_job.rb.rb
+++
@@ -1,7 +0,0 @@
-class ChatMessageCreationEventBroadcastJob < ApplicationJob
-  queue_as :default
-
-  def perform(message)
-    ActionCable.server.broadcast('chat_channel',{created_at: message['created_at'].strftime("%m/%d/%Y at %l:%M:%S%P"), channelId: message.channel.id, id: message[:id], body: message[:body], author_id: message[:author_id]})
-  end
-end
